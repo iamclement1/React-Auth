402760346dfece46dfe6528b32f0d53e97252c30
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-04-18 20:37:31 +0100

    added a nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
-import '../node_modules/boostrap/'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Authentication</h1>
+      <nav className="navbar navbar-expand navbar-light fixed-top">
+        <div className="container">
+          <a href="" className="navbar-brand" 
+          data-toggle="collapse"
+          data-target="#nav">
+            Home
+          </a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="" className="nav-link" 
+                data-toggle="collapse">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link" 
+                data-toggle="collapse">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
