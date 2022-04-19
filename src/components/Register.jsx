be6970082bfe09b8268c0ccea6aaf5e909da1d69
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-04-19 19:36:51 +0100

    installed axios plus used the post request

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,15 +1,37 @@
-
+import { useState } from 'react'
+import axios from 'axios'
 
 export const Register = () => {
 
-    handleSubmit = () => {
-        console.log('Works!')
+    // function handleChange (event) {
+    //     alert(event.target.value);
+    // }
+
+    const [ firstName, setFirstName ] = useState('');
+    const [ lastName, setLastName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [confirmPassword, setConfirmPassword ] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // alert('Your info has been received')
     }
+
+    axios.post('http://localhost:3000/register?', handleSubmit).then(
+        response => {
+            alert(response)
+        }
+    ).catch(
+        error => {
+            console.log(error)
+        }
+    )
 
 
     return (
         <div>
-            <form action="" onSubmit={handleSubmit} >
+            <form onSubmit={handleSubmit} >
                 <h3>
                     Sign Up
                 </h3>
@@ -20,7 +42,11 @@ export const Register = () => {
                     </label>
                     <input type="text"
                     className="form-control"
-                    placeholder="First Name"/>
+                    placeholder="First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={ e => setFirstName(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
@@ -29,7 +55,11 @@ export const Register = () => {
                     </label>
                     <input type="text"
                     className="form-control"
-                    placeholder="Last Name"/>
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange = { e => setLastName(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
@@ -38,7 +68,11 @@ export const Register = () => {
                     </label>
                     <input type="email"
                     className="form-control"
-                    placeholder="Email"/>
+                    placeholder="Email"
+                    name="email"
+                    value={email}
+                    onChange ={ e => setEmail (e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
@@ -47,7 +81,11 @@ export const Register = () => {
                     </label>
                     <input type="password"
                     className="form-control"
-                    placeholder="Password"/>
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={ e => setPassword(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
@@ -56,8 +94,13 @@ export const Register = () => {
                     </label>
                     <input type="password"
                     className="form-control"
-                    placeholder="Confirm Password"/>
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={ e => setConfirmPassword(e.target.value)}
+                    />
                 </div>
+                
                 <button className="btn btn-primary btn-block">
                     Sign Up
                 </button>
